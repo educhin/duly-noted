d0625ebb4b551ecc2b6cf3434c1f35b2da70001f
Educhin <c.s.arinze@gmail.com>
2020-04-06 19:04:41 -0400

    Add functions to fetch and organize clefs to index.js

--- duly-noted-frontend/src/index.js
+++ duly-noted-frontend/src/index.js
@@ -1,9 +1,33 @@
 const BASE_URL = "http://localhost:3000"
 const NOTES_URL = `${BASE_URL}/notes`
 const CLEFS_URL = `${BASE_URL}/clefs`
-const clefNotes = []
+const clefsArray = []
 
 document.addEventListener('DOMContentLoaded', function(){
     fetchClefs()
 });
 
+function fetchClefs() {
+    let obj = {
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        } 
+      }
+    fetch(CLEFS_URL, obj)
+    .then(response => response.json())
+    .then(object => organizeClefs(object))
+    .catch(error => console.log(error.message))
+}
+
+function organizeClefs(obj) {
+    if (obj) {
+        for (clef of obj) {
+            let name = clef.name
+            let notes = clef.notes
+            clefsArray.push(new Clef(name, notes))
+        }
+    }
+    console.log(clefsArray)
+    return clefsArray
+}
